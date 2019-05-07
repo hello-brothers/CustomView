c8dccfa0a3d792d636f5fec9bc89224a1e3438d3
hello-brothers <lh1479784191@qq.com>
2019-05-07 23:01:25 +0800

    second

--- youkucaidan/src/main/java/com/hellobrothers/youkucaidan/Tools.java
+++ youkucaidan/src/main/java/com/hellobrothers/youkucaidan/Tools.java
@@ -1,32 +1,41 @@
 package com.hellobrothers.youkucaidan;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.RotateAnimation;
 import android.widget.RelativeLayout;
 
 public class Tools {
 
-    public static void hideView(View view) {
+    public static void hideView(ViewGroup view) {
         hideView(view, 0);
     }
 
-    public static void showView(View view) {
+    public static void showView(ViewGroup view) {
         showView(view, 0);
     }
 
-    public static void hideView(View view, int startOffset) {
+    public static void hideView(ViewGroup view, int startOffset) {
         RotateAnimation ra = new RotateAnimation(0, 180, view.getWidth()/2, view.getHeight());
         ra.setDuration(500);//设置动画持续时间
         ra.setFillAfter(true);//动画停留在播放完成状态
         ra.setStartOffset(startOffset);
         view.startAnimation(ra);
+        for (int i = 0; i < view.getChildCount(); i++){
+            View children = view.getChildAt(i);
+            children.setEnabled(false);
+        }
     }
 
-    public static void showView(View view, int startOffset) {
+    public static void showView(ViewGroup view, int startOffset) {
         RotateAnimation ra = new RotateAnimation(180, 0, view.getWidth()/2, view.getHeight());
         ra.setDuration(500);//设置动画持续时间
         ra.setFillAfter(true);//动画停留在播放完成状态
         ra.setStartOffset(startOffset);
         view.startAnimation(ra);
+        for (int i = 0; i < view.getChildCount(); i++){
+            View children = view.getChildAt(i);
+            children.setEnabled(true);
+        }
     }
 }
